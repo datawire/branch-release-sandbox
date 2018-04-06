5116475e6edb2ff8b5e5693bd5f856eb6a2e600e
Philip Lombardi <plombardi@datawire.io>
2018-04-06 13:49:32 -0400

    release.sh

---
+++ releng/release.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# file: release.sh
+#
+# This script is responsible for two things:
+#
+# 1. Publishing edge artifacts after a successful build
+# 2. Publishing "release"
+#
+set -o nounset
+set -e errexit
+
+hash="${1:?commit hash is empty or not set}"
+version="${2}"
+
+printf "=== Begin: release.sh args dump ===\n"
+printf "hash    = '$hash'\n"
+printf "version = '$version'\n"
+printf "=== End:   release.sh args dump ===\n"
+
+if [[ -n "${version}" ]]; then
+    printf "Start full stable release operations"
+fi
